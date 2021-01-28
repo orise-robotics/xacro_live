bcbdccf7e6d0ad3d8c074708a33bc5effc3aa8bc
Mateus Amarante Araújo <mateus.amarujo@gmail.com>
2021-01-28 10:13:13 -0300

    Fix flake8 lint problem

--- xacro_live/xacro_update_handler.py
+++ xacro_live/xacro_update_handler.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import typing
-
 import rclpy.logging as roslog
 from watchdog.events import EVENT_TYPE_MODIFIED
 from watchdog.events import FileSystemEventHandler
