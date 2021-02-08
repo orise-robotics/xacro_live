b40a0b2eaa5cc3c30d0cb73cf5dbb645b78c7232
Mateus Amarante Araújo <mateus.amarujo@gmail.com>
2021-02-08 08:59:49 -0300

    Improve XacroTree's attribute access

--- xacro_live/xacro_tree.py
+++ xacro_live/xacro_tree.py
@@ -20,22 +20,38 @@ import xacro
 class XacroTree:
 
     def __init__(self, root_file: str):
-        self.root_file = os.path.realpath(root_file)
-        self.dirs = {os.path.dirname(self.root_file)}
-        self.files = {self.root_file}
-        self.doc = None
+        self.root_file = root_file
+
+    @property
+    def dirs(self):
+        return self._dirs.copy()
+
+    @property
+    def files(self):
+        return self._files.copy()
+
+    @property
+    def root_file(self):
+        return self._root_file
+
+    @root_file.setter
+    def root_file(self, filename):
+        self._root_file = os.path.realpath(filename)
+        self._dirs = {os.path.dirname(self.root_file)}
+        self._files = {self.root_file}
+        self._doc = None
 
     def xml_string(self) -> str:
         """Get current urdf string output of the current version of the target file."""
-        return self.doc.toprettyxml(indent='  ')
+        return self._doc.toprettyxml(indent='  ')
 
     def is_file_member(self, path: str) -> bool:
         """Check if path is a member of the target xacro file tree."""
-        return os.path.realpath(path) in self.files
+        return os.path.realpath(path) in self._files
 
     def update(self) -> None:
         """Process the xacro file and update files & directories."""
-        self.doc = xacro.process_file(
+        self._doc = xacro.process_file(
             self.root_file, **{
                 'output': None,
                 'just_deps': False,
@@ -44,5 +60,5 @@ class XacroTree:
                 'mappings': {}
             }
         )
-        self.files.update([os.path.realpath(file) for file in xacro.all_includes])
-        self.dirs.update([os.path.dirname(file) for file in self.files])
+        self._files.update([os.path.realpath(file) for file in xacro.all_includes])
+        self._dirs.update([os.path.dirname(file) for file in self._files])
